7039ba4d17132c99d4578371cd0090adeafe60c1
mkaufman385 <mkaufman385@gmail.com>
2024-06-21 12:46:43 -0400

    added proper api link and result.data

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -72,7 +72,7 @@ function GraphWrapper(props) {
                                    -- Mack 
     
     */
-
+    //    ADDED result.data
     if (office === 'all' || !office) {
       axios
         .get(process.env.REACT_APP_API_URI, {
@@ -83,7 +83,7 @@ function GraphWrapper(props) {
           },
         })
         .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+          stateSettingCallback(view, office, result.data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
         })
         .catch(err => {
           console.error(err);
@@ -99,7 +99,7 @@ function GraphWrapper(props) {
           },
         })
         .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+          stateSettingCallback(view, office, result.data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
         })
         .catch(err => {
           console.error(err);
